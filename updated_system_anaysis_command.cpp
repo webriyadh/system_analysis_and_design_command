c5336c5ca75fd53569966c6a06a435caccffd821
Riyadul Islam Riyadh <97389821+webriyadh@users.noreply.github.com>
2024-12-01 09:10:38 +0600

    Add files via upload

---
+++ updated_system_anaysis_command.cpp
@@ -0,0 +1,34 @@
+Navigate to Desktop:
+
+cd ~/Desktop
+
+View Files on the Desktop
+
+ls
+
+Change directory 
+
+cd test
+
+
+
+Create a New Script:
+
+
+nano Task_3007.sh
+
+Make the Script Executable
+
+chmod +x Task_3007.sh
+
+
+
+Run the Script from the Desktop
+
+./Task_3007.sh
+
+
+View Script Content:
+
+cat Task_3007.sh
+
